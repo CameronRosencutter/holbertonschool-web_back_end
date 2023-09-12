2a1db1ddef80e86921868bb236d9300528f49e4b
Cameron <120847793+CameronRosencutter@users.noreply.github.com>
2023-09-11 21:31:26 -0500

    Pleasse Work

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,4 @@
-function getStudentsByLocation(students){
-  return students.filter((student) => student.location === 'San Francisco');
+export default function getStudentsByLocation(studentList, city) {
+  const studentsFromCity = studentList.filter((student) => student.location === city);
+  return studentsFromCity;
 }
-
-export default getStudentsByLocation;
